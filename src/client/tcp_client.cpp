ef1689ae8894d577a186fb0527304758015b0b5d
Apoorv Sachan <apoorv.99.sachan@gmail.com>
2024-02-18 03:53:50 +0530

    add client server init functions for tcp

--- src/client/tcp_client.cpp
+++ src/client/tcp_client.cpp
@@ -1,10 +1,15 @@
-#include <sys/socket.h>
 #include "common.h"
+#include <sys/socket.h>
+#include <unistd.h>
+#include <errno.h>
+#include <netdb.h>
+
 
 #define MAXSLEEP 128
+#define BUFLEN  128
 
 int 
-connect_retry(int domain, int type, in tprotocol,
+connect_retry(int domain, int type, int protocol,
               const struct sockaddr *addr, socklen_t alen)
 {
 
@@ -14,7 +19,7 @@ connect_retry(int domain, int type, in tprotocol,
      * Trying to connect with exponential backoff
      */
     
-    for (numsec = 1: numsec <= MAXSLEEP; numsec <<= 1) {
+    for (numsec = 1; numsec <= MAXSLEEP; numsec <<= 1) {
         if ((fd = socket(domain, type, protocol)) < 0)
             return (-1);
         
@@ -38,11 +43,26 @@ connect_retry(int domain, int type, in tprotocol,
     return (-1);
 }
 
-
+void 
+print_uptime(int sockfd)
+{
+    int     n;
+    char    buf[BUFLEN];
+    while ((n = recv(sockfd, buf, BUFLEN, 0)) > 0)
+        write(STDOUT_FILENO, buf, n);
+    if (n < 0)
+        err_quit("recv error");
+}
 // Connect_retry functoin 
 
 int main() {
 
+    struct addrinfo *ailist, *aip;
+    struct addrinfo hint;
+    int sockfd, err;
 
+    if (argc != 2)
+        err_quit("usage arguments - <hostname>");
+    memset (&hint, 0, )
     return 0;
 }
